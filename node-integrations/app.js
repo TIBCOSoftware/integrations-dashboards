0074686b76e920b61664624df77676b260d931ec
Christian Florez <cflorez@tibco.com>
2019-05-06 13:10:25 -0600

    begin implementing new deployment logic

--- node-integrations/app.js
+++ node-integrations/app.js
@@ -1,5 +1,4 @@
-// require('custom-env').env(process.env.NODE_ENV || 'development');
-require('custom-env').env();
+require('custom-env').env(process.env.NODE_ENV || 'development');
 const tasks = require('./tasks');
 const scheduler = require('./util/scheduler');
 const { logInfo } = require('./util/logger');
@@ -7,14 +6,16 @@ const { logInfo } = require('./util/logger');
 const main = async () => {
   logInfo('Node Integrations Initialized');
 
-  scheduler.schedule(
-    scheduler.cron.every30Minutes,
-    async () => {
-      await tasks.boss2Jefe.assetIntegration.run();
-    },
-    'BOSS2JEFE Asset Integration'
-  );
-  // console.log(process.env);
+  // scheduler.schedule(
+  //   scheduler.cron.every30Minutes,
+  //   async () => {
+  //     await tasks.boss2Jefe.assetIntegration.run();
+  //   },
+  //   'Initialize BOSS2JEFE Asset Integration'
+  // );
+  logInfo('########################################');
+  logInfo(process.env.FOO_BAR);
+  logInfo('########################################');
 };
 
 main();
--- node-integrations/build.js
+++ node-integrations/build.js
@@ -1,19 +1,44 @@
 const fs = require('fs');
-const AdmZip = require('adm-zip');
+const _ = require('lodash');
 const manifest = require('./manifest.json');
+const taskList = require('./tasks/taskList');
 
 const buildAgent = {};
 
+/**
+ * Run with npm run build -- {environment}
+ * Environment can be development, sandbox, or production
+ * Defaults to development
+ */
 buildAgent.init = () => {
-  const taskName = process.argv[2];
-  buildAgent.createZipFile(taskName);
+  const environment = process.argv[2] || 'development';
+
+  if (!['development', 'production', 'sandbox'].includes(environment)) {
+    console.log('Please specify one of the following environments: development, production, sandbox');
+    return;
+  }
+
+  _.map(taskList, taskName => {
+    buildAgent.createZipFile(taskName, environment);
+  });
 };
 
-buildAgent.createZipFile = taskName => {
-  const zip = new AdmZip();
+buildAgent.createZipFile = (taskName, environment) => {
+  console.log(taskName, environment);
 
-  const files = fs.readdirSync('./');
-  console.log(files);
+  /**
+   * 1. First, determine the correct .env file
+   * 2. Write TASK_NAME to .env file
+   * 3. then run bash zip command, send to deploy/${taskName}/app.zip
+   */
+};
+
+buildAgent.createManifestJson = taskName => {
+  /**
+   * 1. update name based on taskName
+   * 2. write to deploy/${taskName}/manifest.json
+   *
+   */
 };
 
 buildAgent.init();
