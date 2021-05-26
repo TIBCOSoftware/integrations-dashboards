2532f20700c1fa79be59b047f5b597692ea43a75
build-Fitbit <ali.fakhraei@tibco.com>
2021-05-26 14:43:50 -0600

    added close call to close browser

--- selenium-regression-test-quote/quote_line_test/test.js
+++ selenium-regression-test-quote/quote_line_test/test.js
@@ -110,6 +110,7 @@ export const quotelineTest = async(quoteId, ownerId, approverId, operationId, qu
     // change opp fields
     await sales_complete(quoteId, operationId, driver);
     
+    driver.close();
     driver.quit();
 }
  
